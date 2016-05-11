d48691e3450403c66f582aa97d42c0017013bc79
sikB <sikhovski@icloud.com>
2016-05-11 11:30:15 -0400

    Six flags WW mock with php
    init

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>White Water PhP</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<div id="header">
+		<div id="header-left">
+			<div id="logo">
+				<img src="img/default-sf-logo.png">
+			</div>
+		</div>
+		<div id="header-right">
+			<div id="header-links">
+				<a href="#"><li>Park Home </li></a>
+				<a href="#"><li> National Home </li></a>
+				<a href="#"><li> Park Map </li></a>
+				<a href="#"><li> Pass Holder Login </li></a>
+				<a href="#"><li class="cart"> <img src="https://www.sixflags.com/sites/all/themes/sixflags/images/icons/icon-cart.png"> My Cart </li></a>
+				<a href="#"><li class="promo"> Enter Promo Code</li></a>
+			</div>
+			<div class="sfww">
+				<p class="pTag">Six Flags White Water</p>
+				<p class="atl">Atlanta</p>
+			</div>
+		</div>
+	</div>
+	<div class="navigation">
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		    </div>
+		    <ul class="nav navbar-nav col-sm-12">
+		      <li class="dropdown one col-sm-offset-1"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Things To Do <span class="caret"></span></a>
+		        <ul class="dropdown-menu">
+		          <li><a href="#">All rides</a></li>
+		          <li><a href="#">Thrill rides</a></li>
+		          <li><a href="#">family rides</a></li>
+		          <li><a href="#">kids rides</a></li>
+		          <li><a href="#">Dining</a></li>
+		          <li><a href="#">Shopping</a></li>
+		        </ul>
+		      </li>
+		 <li class="dropdown two"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Plan visit <span class="caret"></span></a>
+		        <ul class="dropdown-menu">
+		          <li><a href="#">Directions</a></li>
+		          <li><a href="#">Park Map</a></li>
+		          <li><a href="#">Espanol</a></li>
+		          <li><a href="#">Special offers</a></li>
+		          <li><a href="#">Lost and Found</a></li>
+		          <li><a href="#">Lodging</a></li>
+		          <li><a href="#">Guest Relations</a></li>
+		          <li><a href="#">Park Policy</a></li>
+		          <li><a href="#">Park Services</a></li>
+		          <li><a href="#">Tips for family fun</a></li>
+		          <li><a href="#">Guest Safety</a></li>
+		          <li><a href="#">FAQs</a></li>
+		        </ul>
+		      </li>
+		      <li class="two"><a href="#">Whats New</a></li>
+		      <li class="two"><a href="#">Events</a></li>
+		      <li class="two"><a href="#">Park Hours</a></li>
+		<li class="dropdown two"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Groups <span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+		          <li><a href="#">Birthday Parties</a></li>
+		          <li><a href="#">Youth Groups</a></li>
+		          <li><a href="#">School Groups</a></li>
+		          <li><a href="#">Religious Organizations</a></li>
+		          <li><a href="#">Reunions</a></li>
+		          <li><a href="#">Travel Industry</a></li>
+		          <li><a href="#">Other Groups</a></li>
+		          <li><a href="#">Small Groups(15-49)</a></li>
+		          <li><a href="#">Large Groups(50+)</a></li>
+		        </ul>
+		      </li>
+		<li class="dropdown two passes"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Passes & Memberships<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+		          <li><a href="#">Buy Passes</a></li>
+		          <li><a href="#">Buy Memberships</a></li>
+		          <li><a href="#">Buy Dining Passes</a></li>
+		          <li><a href="#">Membership FAQs</a></li>
+		          <li><a href="#">Season Dining FAQs</a></li>
+		          <li><a href="#">Pass Holder Perks</a></li>
+		          <li><a href="#">Manage Your Pass Benefits</a></li>
+		          <li><a href="#">Make a Payment</a></li>
+		        </ul>
+		      </li>
+		<li class="two passes"><a href="#">Tickets</a></li>
+		    </ul>
+		  </div>
+		</nav>
+	</div>
+</body>
+</html>
